a08d32314c72819c79778d4adfb67e2d0f8838f2
Katie Heinemann <katherine.heinemann@nasa.gov>
2022-01-24 22:32:38 -0500

    feat(tests): Add unit test for an api function

---
+++ src/__tests__/Api.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {getShippingInfoByID} from '../api/Api';
+
+
+// Example of a unit test, individual function
+test('gets shipping info from api fallback by id', () => {
+
+  const expected = {
+    "id": 4,
+    "label_id": "94-748-4437",
+    "shipping_tracking_code": "7763306017"
+  };
+
+  getShippingInfoByID('94-748-4437').then((response) => {
+    expect(response).toBe(expected);
+  })
+});
--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../components/App/App';
 
+// Example of a component test
 test('renders welcome message', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Welcome to the Biobot Test Kit Search App!/i);
+  const linkElement = screen.getByText(/Welcome to the Biobot Test Kit Search App/i);
   expect(linkElement).toBeInTheDocument();
 });
