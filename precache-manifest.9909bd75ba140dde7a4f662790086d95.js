b9d25140d69d91fc246102c9c75ee3fd60d50cc6
rocketsciencenerd <rocketsciencenerd@users.noreply.github.com>
2022-01-25 03:33:54 +0000

    deploy: a08d32314c72819c79778d4adfb67e2d0f8838f2

---
+++ precache-manifest.9909bd75ba140dde7a4f662790086d95.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8147cc95181fb7b179da1598f5ebd1a7",
+    "url": "/biobot-search/index.html"
+  },
+  {
+    "revision": "b6d9c89bdbff556484ca",
+    "url": "/biobot-search/static/css/2.b8803beb.chunk.css"
+  },
+  {
+    "revision": "b55f199f3ecd9b1dba58",
+    "url": "/biobot-search/static/css/main.47de604d.chunk.css"
+  },
+  {
+    "revision": "b6d9c89bdbff556484ca",
+    "url": "/biobot-search/static/js/2.2bfd242a.chunk.js"
+  },
+  {
+    "revision": "9083ca2f110689bab9a9",
+    "url": "/biobot-search/static/js/3.ee2579e7.chunk.js"
+  },
+  {
+    "revision": "b55f199f3ecd9b1dba58",
+    "url": "/biobot-search/static/js/main.e327c0ca.chunk.js"
+  },
+  {
+    "revision": "961c0ce8537f65ef8bdf",
+    "url": "/biobot-search/static/js/runtime~main.8e5ccd9d.js"
+  },
+  {
+    "revision": "34eda38920a36fb9cb22d54563b199bb",
+    "url": "/biobot-search/static/media/logo.34eda389.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2776276f23afa75cd1eaed4e91afcde5.js"
+  "/biobot-search/precache-manifest.9909bd75ba140dde7a4f662790086d95.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/biobot-search/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
