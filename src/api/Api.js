926dca84e6718aad703081e5c78baa4d73e7ec95
Katie Heinemann <katherine.heinemann@nasa.gov>
2022-01-24 20:53:50 -0500

    feat(api): Add Api

---
+++ src/api/Api.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import {shippingData} from '../data/KITS_SHIPPING_DATA_FALLBACK';
+
+const testKitDataApiUrl = 'http://localhost:4000/shipping_data';
+
+export const getTestKitShippingInfo = async() => {
+  return axios.get(testKitDataApiUrl).then((resp) => {
+    const { data } = resp || {};
+    return data;
+  }).catch(() => {
+    console.error('Api unavailable, loading from JSON ...');
+    return shippingData;
+  });
+}
+
+export const getShippingInfoByID = async(id) => {
+  const url = `${testKitDataApiUrl}?label_id=${id}`;
+  return axios.get(url).then((resp) => {
+    const { data } = resp || {};
+    return data;
+  }).catch(() => {
+    console.error('Api unavailable, loading from JSON ...');
+    return shippingData.find(o => o.id === id);
+  });
+}
+
+
+export default {getTestKitShippingInfo, getShippingInfoByID};
